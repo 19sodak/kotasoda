83d30daad28399fa1b950c9df9edfedb5bfa38d9
Kota Soda <kota_soda@brown.edu>
2020-07-30 17:41:50 +0900

    files

---
+++ data.js
@@ -0,0 +1,38 @@
+var data =  
+   [
+        {
+            "title": "Wiki Search Engine",
+            "description": "Scala search engine that indexes and queries search files",
+            "techStack": "Scala"
+        },
+        {
+            "title": "Brown Q&A",
+            "description": "Reddit like forum for Brown University Students",
+            "techStack": "handlebars.js, Javascript, CSS, Bootstrap, MariaDB, Node.js"
+        },
+        {
+            "title": "U4Ea Binaural Beats App",
+            "description": "Binaural Beats webapp/player",
+            "techStack": "React.js, CSS, Bootstrap, Azure"
+        },
+        {
+            "title": "Connect4 AI",
+            "description": "Connect4 AI using minimax algorithm",
+            "techStack": "ReasonML"
+        },
+        {
+            "title": "Ticketeer",
+            "description": "Event sharing and ticket registration website",
+            "techStack": "react.js, Node.js, Express.js, MongoDB"
+        },
+        {
+            "title": "Brown Ask Portal",
+            "description": "coming soon!",
+            "techStack": "coming soon!"
+        },
+        {
+            "title": "Personal Website",
+            "description": "Personal Website",
+            "techStack": "HTML, CSS, JavaScript, Bootstrap"
+        }
+    ]
